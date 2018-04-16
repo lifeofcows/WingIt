b6c7d27ca8ae57a7236a50c5ccd93121528d14c7
Avery Vine <averyvine@icloud.com>
2018-04-15 22:35:22 -0400

    added client-side portion of recommendation system

--- Client/public/scripts.js
+++ Client/public/scripts.js
@@ -2,22 +2,78 @@ $(document).ready(function() {
     let urlPrefix = "/?url=";
 
     $(".searchButton").on("click", function() {
-        $(".results").css("display", "none");
+        clearData();
         let targetURL = $(".searchTerm").val();
         if (targetURL != "") {
             console.log("Requesting url: " + targetURL);
             $.get(urlPrefix + targetURL, function(data) {
-                console.log("Received data with status code: " + data.statusCode);
-                $(".results").css("display", "inline-block");
-                if (data.statusCode == 200 && data.wing) {
-                    $(".resultsText").text("Wing: " + data.wing);
+                if (data.statusCode) {
+                    console.log("Received data with status code: " + data.statusCode);
+                    // presentData(data);
+                    var temp = {"statusCode": 200,
+                                "wing": "left",
+                                "recommendations": {
+                                    "left": ["link1", "link2", "link3"],
+                                    "centrist": ["link1", "link2", "link3"],
+                                    "right": ["link1", "link2", "link3"]
+                                }
+                            }
+                    presentData(temp);
                 } else {
-                    $(".resultsText").text("Something went wrong!");
+                    presentData("{\"statusCode\": \"500\"}");
                 }
             });
         } else {
             alert("Psst... you might want to enter a query!");
         }
     });
+
+    function presentData(data) {
+        console.log("Presenting data");
+        $(".results").css("display", "inline-block");
+        if (data.statusCode == 200 && data.wing) {
+            console.log("Valid status code and wing");
+            if (data.wing == "left" || data.wing == "right") {
+                $(".resultsText").text("This article is " + data.wing.toUpperCase() + "-LEANING");
+            } else {
+                $(".resultsText").text("This article is " + data.wing.toUpperCase());
+            }
+
+            if (data.recommendations) {
+                console.log("Recommendations found");
+
+                var html = "<table border style=\"margin-left: auto; margin-right: auto; width: 80%;\"> <tr> <th> Wing </th> <th> Recommendation </th> </tr> ";
+
+                Object.keys(data.recommendations).forEach(function(recommendationWing) {
+
+                    console.log("Wing: " + recommendationWing);
+                    console.log("Data: " + data.recommendations[recommendationWing]);
+                    html += "<tr> <td> " + recommendationWing + " </td> <td> <ul> ";
+
+                    data.recommendations[recommendationWing].forEach(function(recommendationWing) {
+
+                        console.log("Wing: " + recommendationWing);
+                        html += "<li> " + recommendationWing + " </li> ";
+
+                    });
+
+                    html += "</tr> ";
+                    
+                });
+
+                html += "</table>";
+
+                $(".recommendations-table").html(html);
+            }
+
+        } else {
+            $(".resultsText").text("Something went wrong! Please try again later.");
+        }
+    }
+
+    function clearData() {
+        $(".recommendations-table").html("");
+        $(".results").css("display", "none");
+    }
 
 });
--- Server/src/edu/carleton/comp4601/resources/Recommender.java
+++ Server/src/edu/carleton/comp4601/resources/Recommender.java
@@ -3,6 +3,7 @@ package edu.carleton.comp4601.resources;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -31,11 +32,11 @@ public class Recommender {
 	static {
 		newsSites = new HashMap<String, String>();
 		newsSites.put("https://www.vox.com/", WingAnalyzer.left);
-//		newsSites.put("https://www.buzzfeed.com/", WingAnalyzer.left);
+		newsSites.put("https://www.buzzfeed.com/", WingAnalyzer.left);
 		newsSites.put("https://www.economist.com/", WingAnalyzer.neutral);
-//		newsSites.put("https://www.reuters.com/", WingAnalyzer.neutral);
+		newsSites.put("https://www.reuters.com/", WingAnalyzer.neutral);
 		newsSites.put("https://www.infowars.com/", WingAnalyzer.right);
-//		newsSites.put("www.drudgereport.com/", WingAnalyzer.right);
+		newsSites.put("www.drudgereport.com/", WingAnalyzer.right);
 		
 	}
 	private static final int NUM_THREADS = 3;
@@ -66,7 +67,10 @@ public class Recommender {
 					train();	
 				}
 				break;
-			case "analysis":
+			case "re-train":
+				train();
+				break;
+			case "analytics":
 				analysis();
 				break;
 			default:
@@ -161,6 +165,20 @@ public class Recommender {
 	
 	public static String JSONify(String key, String value, boolean wrap) {
 		String json = "\"" + key + "\": \"" + value + "\"";
+		if (wrap) {
+			return wrapJSON(json);
+		}
+		return json;
+	}
+	
+	public static String JSONify(String key, List<String> list, boolean wrap) {
+		String json = "\"" + key + "\": \"[";
+		for (int i = 0; i < list.size(); i++) {
+			json += "\"" + list.get(i) + "\"";
+			if (i < list.size() - 1) {
+				json += ", ";
+			}
+		}
 		if (wrap) {
 			return wrapJSON(json);
 		}
